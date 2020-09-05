f0fbca9d505e319515acaea16c6b159e7d23b7fe
Mikel Bishop <mdrew8@gmail.com>
2020-09-05 01:30:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,9 @@ class School
   
   def add_student(student, grade)
     if roster.has_key?(grade)
-    roster[grade] = []
     roster[grade] << student 
+  else 
+    roster[grade] = [name]
   end 
   
 end 
