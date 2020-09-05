88d17b07510bad8e88449316e8acab402c9f7843
Mikel Bishop <mdrew8@gmail.com>
2020-09-05 00:43:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,12 @@ require 'pry'
 
 class School
   
+  roster = {}
+  
   attr_accessor :school 
   
   def initialize(school)
     @school = school
-    roster = {} 
   end 
   
   def add_student(name, grade)
